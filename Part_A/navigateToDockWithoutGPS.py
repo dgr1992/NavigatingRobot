e2c543823da7de612bd1e67743140d7a784e821c
Nepomuk1999 <christoph.bauer@students.fhv.at>
2018-11-15 16:49:01 +0100

    Aufgabe 2 Odometrie initial commit inkl berechnung

--- Part_A/navigateToDockWithoutGPS.py
+++ Part_A/navigateToDockWithoutGPS.py
@@ -7,7 +7,8 @@ import math
 
 def main():
     robot = EPuckVRep('ePuck', port=19999, synchronous=False)
-
+    robot.enablePose()
+    robot.enableWheelEncoding()
     # xStart = 0.5
     # yStart = 0.5
     # zetaStart = 0
@@ -19,49 +20,54 @@ def main():
     goalPose = np.matrix([[xGoal], [yGoal], [zetaGoal]])
 
     # Control parameters
-    kRoh = 1
-    kAlpha = 1
-    kBeta = 1
+    kRoh = 0.5
+    kAlpha = 5
+    kBeta = -3.5
     r = 0.0425  # in meters
     l = 0.0541  # in meters
 
     # Define the control matrix
-    c00 = kRoh / r
-    c01 = (kAlpha * l) / r
-    c02 = (kBeta * l) / r
-    c10 = kRoh / r
-    c11 = -(kAlpha * l) / r
-    c12 = -(kBeta * l) / r
-    K = np.matrix([[c00, c01, c02], [c10, c11, 12]])
+    c00 = kRoh/r
+    c01 = (kAlpha*l)/r
+    c02 = (kBeta*l)/r
+    c10 = kRoh/r
+    c11 = -(kAlpha*l)/r
+    c12 = -(kBeta*l)/r
+    K = np.matrix([[c00,c01,c02],[c10,c11,c12]])
+
     currentPose = robot._getPose()
 
     # main sense-act cycle
     while robot.isConnected():
-        motorSpeed = K * polarTransf(currentPose, goalPose)
+        pol = polarTransf(currentPose, goalPose)
+        print pol
+        motorSpeed = K * pol
 
         robot.setMotorSpeeds(motorSpeed[0][0], motorSpeed[1][0])
 
         time.sleep(0.05)
         #TODO prüfen ob die Werte der distanz, der summe der disanz oder etwas anderem entsprechen siehe skriptum merz s20
         driven = robot.getWheelEncodingValues()
-        currentPose = calcCurrentPos(currentPose, robot._wheelDistance, driven[1], driven[2])
+
+        currentPose = calcCurrentPos(currentPose, robot._wheelDistance, driven[0], driven[1])
 
     robot.disconnect()
 
 def calcCurrentPos(oldPos, wheelBase, drivenSr, drivenSl):
-    c00 = (drivenSr + drivenSl)/2*math.cos(oldPos[2][0] + ((drivenSr-drivenSl)/(2*wheelBase)))
-    c10 = (drivenSr + drivenSl)/2*math.sin(oldPos[2][0] + ((drivenSr-drivenSl)/(2*wheelBase)))
+    c00 = (drivenSr + drivenSl)/2*math.cos(oldPos[2] + ((drivenSr-drivenSl)/(2*wheelBase)))
+    c10 = (drivenSr + drivenSl)/2*math.sin(oldPos[2] + ((drivenSr-drivenSl)/(2*wheelBase)))
     c20 = (drivenSr-drivenSl)/wheelBase
     temp = np.matrix([[c00], [c10], [c20]])
     return oldPos + temp
 
-def polarTransf(currentPose, goalPose):
-    deltaX = goalPose[0][0] - currentPose[0][0]
-    deltaY = goalPose[1][0] - currentPose[1][0]
-    roh = math.sqrt(deltaX ** 2 + deltaY ** 2)
-    alpha = - currentPose[2][0] + math.atan2(deltaY, deltaX)
-    beta = - currentPose[2][0] - alpha
-    return np.matrix([[roh], [alpha], [beta]])
+def polarTransf(current, goal):
+    deltaX = goal[0][0] - current[0]
+    deltaY = goal[1][0] - current[1]
+    temp = (deltaX[0][0])**2 + (deltaY[0][0])**2
+    roh = math.sqrt(temp)
+    alpha = - current[2] + math.atan2(deltaY,deltaX)
+    beta = - current[2] - alpha
+    return np.matrix([[roh],[alpha],[beta]])
 
 
 if __name__ == '__main__':
